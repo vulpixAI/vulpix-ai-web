dd3ed4442778cd713b414e48afae86c20db625ea
leOhsantos <leonardo.santos@jazztech.com.br>
2024-08-19 13:05:15 -0300

    feat: adicionando svg de waves na home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,19 +8,29 @@ export default function Home() {
     useLastPage();
 
     const [wasScrolled, setWasScrolled] = useState<Boolean>(false);
-    const [navBackgroundColor, setNavBackgroundColor] = useState<String>("bg-black");
+    const [navBackgroundColor, setNavBackgroundColor] = useState<String>("transparent");
 
     useEffect(() => {
         window.addEventListener("scroll", () => window.scrollY > 0 ? setWasScrolled(true) : setWasScrolled(false));
         AOS.init();
     }, []);
 
-    useEffect(() => wasScrolled ? setNavBackgroundColor("bg-gray") : setNavBackgroundColor("bg-black"), [wasScrolled]);
+    useEffect(() => wasScrolled ? setNavBackgroundColor("bg-gray/85") : setNavBackgroundColor("transparent"), [wasScrolled]);
 
     return (
         <div className='h-screen bg-black'>
             <header id="header" className="bg-black">
-                <nav className={"fixed w-full flex justify-center items-center h-20 top-0 transition-all duration-300 " + navBackgroundColor}>
+                <div className={"absolute w-full flex justify-center items-center h-20 top-0"}>
+                    <div className='w-1/4'></div>
+                    <div className="w-1/2 mx-12"></div>
+                    <div className="w-1/4 flex">
+                        <div className='w-[1000px] h-[650px] overflow-hidden'>
+                            <img src="src/assets/waves.svg" className="scale-[1.7] mt-[320px] ml-[70px] select-none pointer-events-none" />
+                        </div>
+                    </div>
+                </div>
+
+                <nav className={"fixed z-10 w-full flex justify-center items-center h-20 top-0 transition-all duration-300 " + navBackgroundColor}>
                     <div className='w-1/4 flex justify-end'>
                         <img className="h-[4.5rem] pointer-events-none" src="/vulpixai-logo.jpeg" alt="Logo vulpix.AI" />
                     </div>
@@ -34,13 +44,15 @@ export default function Home() {
                     </ul>
 
                     <div className="w-1/4 flex items-center">
-                        <Link to="/login" className="h-8 mr-4 text-white-gray hover:text-purple transition-all flex items-center">Entrar</Link>
-                        <Link to="/signup" className="h-8 px-3 bg-purple flex items-center text-white rounded-sm hover:bg-purple-dark transition-all">Inscrever-se</Link>
+                        <div className='relative flex w-[172px]'>
+                            <Link to="/login" className="h-8 mr-4 text-white-gray hover:text-purple transition-all flex items-center">Entrar</Link>
+                            <Link to="/signup" className="h-8 px-3 text-nowrap bg-purple flex items-center text-white rounded-sm hover:bg-purple-dark transition-all">Inscrever-se</Link>
+                        </div>
                     </div>
                 </nav>
 
                 <div className="flex flex-col justify-center items-center pt-20 text-white-gray">
-                    <h1 className="text-4xl font-bold mt-16">Seu agente de marketing.</h1>
+                    <h1 className="text-4xl font-bold mt-20">Seu agente de marketing.</h1>
                     <p className="mt-4 text-md">Impulsione suas vendas com IA: personalize campanhas, <br /> segmente públicos com precisão e automatize interações.</p>
                     <div className="bg-white h-[450px] w-[75%] mt-20 rounded-sm"></div>
                 </div>
