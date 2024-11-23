2c49d2f2e37d779890540cfd782ad3dc25acd530
leOhsantos <leonardo.santos191004@gmail.com>
2024-11-23 01:40:57 -0300

    feat: sistema de paginação em publicações

--- src/pages/Application/Post.tsx
+++ src/pages/Application/Post.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Menu } from "../../components/Menu";
 import { Input } from "../../components/Input";
 import { Modal } from "../../components/Modal";
@@ -6,6 +6,7 @@ import { formatMonth } from "../../utils/dateUtils";
 import { padZero } from "../../utils/stringUtils";
 import { Skeleton } from "@mui/material";
 import dayjs, { Dayjs } from "dayjs";
+import CircularProgress from '@mui/material/CircularProgress';
 import InfoIcon from '@mui/icons-material/Info';
 import Tooltip from '@mui/material/Tooltip';
 import EventIcon from '@mui/icons-material/Event';
@@ -18,6 +19,10 @@ interface postResponse {
 }
 
 export default function Posts() {
+    const postsContainer: any = useRef(null);
+    const isLastPage = useRef(false);
+    const isPaginationLoading = useRef(false);
+
     const [posts, setPosts] = useState<postResponse[]>([]);
     const [isLoading, setLoading] = useState<boolean>(true);
     const [hasPost, setHasPost] = useState<boolean>(true);
@@ -31,20 +36,38 @@ export default function Posts() {
     const openDateRangePickerModal = () => { setStartDate(selectedStartDate), setEndDate(selectedEndDate), setDateRangePickerModalOpen(true) };
     const closeDateRangePickerModal = () => setDateRangePickerModalOpen(false);
 
+    const [page, setPage] = useState<number>(0);
+
+    function handleScroll() {
+        if (isLastPage.current) return;
+        if (isPaginationLoading.current) return;
+
+        if (postsContainer.current.clientHeight + postsContainer.current.scrollTop + 1 >= postsContainer.current.scrollHeight) {
+            isPaginationLoading.current = true;
+            setPage(prev => prev + 1);
+        }
+    }
+
     useEffect(() => {
-        axios.get(`${import.meta.env.VITE_API_URL}/posts`, {
+        setTimeout(() => postsContainer.current.addEventListener("scroll", handleScroll), 200);
+    }, []);
+
+    useEffect(() => {
+        axios.get(`${import.meta.env.VITE_API_URL}/posts?page=${page}&size=10`, {
             headers: {
                 Authorization: `Bearer ${sessionStorage.getItem("bearerToken")}`
             }
         }).then(response => {
-            setPosts(response.data);
+            page == 0 ? setPosts(response.data.content) : setPosts(prev => [...prev, ...response.data.content]);
             setLoading(false);
+            isPaginationLoading.current = false;
+            response.data.last && (isLastPage.current = true);
         }).catch((err) => {
             if (err.response.status == 404) {
                 setTimeout(() => { setLoading(false), setHasPost(false) }, 3000);
             }
         });
-    }, []);
+    }, [page]);
 
     function filterDate() {
         setSelectedStartDate(startDate);
@@ -62,7 +85,7 @@ export default function Posts() {
                         {formatMonth(new Date(selectedEndDate).getMonth())} {padZero(new Date(selectedEndDate).getDate())}, {new Date(selectedEndDate).getFullYear()}
                     </button>
                 </div>
-                <div className={`${!isLoading && "overflow-x-hidden"} ${hasPost ? "grid grid-cols-2" : "flex justify-center"} px-2 h-4/5 pb-8`}>
+                <div ref={postsContainer} className={`${!isLoading && "overflow-x-hidden"} ${hasPost ? "grid grid-cols-2" : "flex justify-center"} px-2 h-4/5`}>
                     {isLoading
                         ?
                         <>
@@ -105,17 +128,24 @@ export default function Posts() {
                         </>
                         :
                         hasPost
-                            ? posts.map((post: postResponse, index: number) =>
-                                <div key={index} className={`relative flex items-center justify-between ${index != 0 && index != 1 && "mt-12"} bg-dark-gray rounded-2xl pr-6 mx-4 w-[41rem]`}>
-                                    <img className="w-[340px] h-[300px] rounded-2xl" src={post.media_url} />
-                                    <p className="flex justify-start ml-8 text-white-gray w-[460px] h-[122px] overflow-hidden">{post.caption}</p>
-                                    <span className="text-white-gray absolute bottom-4 right-4 hover:text-purple transition-colors cursor-pointer">
-                                        <Tooltip title="Informações" placement="bottom">
-                                            <InfoIcon />
-                                        </Tooltip>
-                                    </span>
+                            ?
+                            <>
+                                {posts.map((post: postResponse, index: number) =>
+                                    <div key={index} className={`relative flex items-center justify-between ${index != 0 && index != 1 && "mt-12"} bg-dark-gray rounded-2xl pr-6 mx-4 w-[41rem] h-[300px]`}>
+                                        <img className="w-[340px] h-[300px] rounded-2xl" src={post.media_url} />
+                                        <p className="flex justify-start ml-8 text-white-gray w-[460px] h-[122px] overflow-hidden">{post.caption}</p>
+                                        <span className="text-white-gray absolute bottom-4 right-4 hover:text-purple transition-colors cursor-pointer">
+                                            <Tooltip title="Informações" placement="bottom">
+                                                <InfoIcon />
+                                            </Tooltip>
+                                        </span>
+                                    </div>
+                                )
+                                }
+                                <div className="flex justify-end w-full my-8">
+                                    {isPaginationLoading.current && <CircularProgress size="50px" sx={{ color: "#5d5aff", marginRight: "-25px" }} />}
                                 </div>
-                            )
+                            </>
                             : <h1 className="text-white-gray text-xl mt-8">Ooops! Parece que ainda não tem nada por aqui. Que tal publicar algo e inaugurar o espaço? 😄</h1>
                     }
                 </div>
